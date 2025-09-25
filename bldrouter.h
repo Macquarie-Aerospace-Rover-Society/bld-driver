942b74a704615f59324742953df8f68623750b4a
Adrian Kane <egg_on_stilts@hotmail.com>
2025-09-25 20:34:55 +1000

    Updated HTML with bens hint

--- bldrouter.h
+++ bldrouter.h
@@ -36,6 +36,7 @@ void setupWebServer(ControlCallback onControl) {
       <html lang="en">
       <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Robot Controller</title>
         <style>
           body { font-family: Arial, sans-serif; text-align: center; margin-top: 50px; }
